cbe6b043309910c6021165da714830fa6ae8258f
Karmazynowy121 <atak121@wp.pl>
2023-01-27 20:08:49 +0100

    up:add id to  trainingscheuldedto

--- Dto/TrainingScheuldeDto.cs
+++ Dto/TrainingScheuldeDto.cs
@@ -2,6 +2,7 @@
 {
     public class TrainingScheuldeDto
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public UserTrainingScheuldeDto User { get; set; }
         public List<TrainingDto> Trainings { get; set; }
