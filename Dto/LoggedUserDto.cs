2315646d6ae52f3bbffa0000723077c46c3f3bfc
Karmazynowy121 <atak121@wp.pl>
2023-02-01 00:10:03 +0100

    up: add role for user

--- Dto/LoggedUserDto.cs
+++ Dto/LoggedUserDto.cs
@@ -5,6 +5,7 @@
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string Role { get; set; } = "user";
         public string Jwt { get; set; }
     }
 }
