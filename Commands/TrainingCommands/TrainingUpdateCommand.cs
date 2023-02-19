51e213a32b783bc7dbdd189983ee5540fcf3291e
Karmazynowy121 <atak121@wp.pl>
2023-02-19 19:23:08 +0100

    Training update command - Training scheulde Id bugg fixed

--- Commands/TrainingCommands/TrainingUpdateCommand.cs
+++ Commands/TrainingCommands/TrainingUpdateCommand.cs
@@ -7,6 +7,6 @@
         public DateTime TrainingDate { get; set; }
         public int Interval { get; set; }
         public int TrainingDuration { get; set; } = 0;
-        public int TrainingScheudleId { get; set; }
+        public int TrainingScheuldeId { get; set; }
     }
 }
